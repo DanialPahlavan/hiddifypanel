feb957c762c7638e92b04063d9752f9dc053bbc9
Dany P <1260701+DanialPahlavan@users.noreply.github.com>
2026-06-25 01:08:55 +0330

    chore: bump panel version to 12.3.5

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,6 +1,6 @@
 # import importlib.metadata
 from datetime import datetime
 
-__version__ = '12.3.4'
-__release_time__= datetime.strptime('2026-06-24T23:20:00','%Y-%m-%dT%H:%M:%S')
+__version__ = '12.3.5'
+__release_time__= datetime.strptime('2026-06-25T01:10:00','%Y-%m-%dT%H:%M:%S')
 is_released_version = False
